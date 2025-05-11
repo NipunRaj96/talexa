a8fa27182743ca037323e9d64dd8dbfb0182952b
nipunraj2004 <nipunraj2004@gmail.com>
2025-05-11 09:13:34 +0000

    commit

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -36,9 +36,11 @@ const Footer: React.FC = () => {
               <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5229 6.47715 22 12 22C17.5229 22 22 17.5229 22 12C22 6.47715 17.5229 2 12 2ZM12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4ZM9 11.5C9 10.1193 10.1193 9 11.5 9H14.5C15.8807 9 17 10.1193 17 11.5V14.5C17 15.8807 15.8807 17 14.5 17H11.5C10.1193 17 9 15.8807 9 14.5V11.5ZM11.5 11C11.2239 11 11 11.2239 11 11.5V14.5C11 14.7761 11.2239 15 11.5 15H14.5C14.7761 15 15 14.7761 15 14.5V11.5C15 11.2239 14.7761 11 14.5 11H11.5Z" fill="currentColor"/>
             </svg>
           </a>
-          <a href="https://medium.com" target="_blank" rel="noopener noreferrer" className="hover:opacity-70 transition-opacity">
+          <a href="https://your-portfolio-url.com" target="_blank" rel="noopener noreferrer" className="hover:opacity-70 transition-opacity">
             <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fillRule="evenodd" clipRule="evenodd" d="M5 4H19C19.5523 4 20 4.44772 20 5V19C20 19.5523 19.5523 20 19 20H5C4.44772 20 4 19.5523 4 19V5C4 4.44772 4.44772 4 5 4ZM2 5C2 3.34315 3.34315 2 5 2H19C20.6569 2 22 3.34315 22 5V19C22 20.6569 20.6569 22 19 22H5C3.34315 22 2 20.6569 2 19V5ZM7 7H10V17H7V7ZM13 7H17V17H13V7Z" fill="currentColor"/>
+              <path d="M19 7H5C3.89543 7 3 7.89543 3 9V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V9C21 7.89543 20.1046 7 19 7ZM5 9V17H19V9H5Z" fill="currentColor"/>
+              <path d="M10 12H14V14H10V12Z" fill="currentColor"/>
+              <path d="M15 5H9C8.44772 5 8 5.44772 8 6V7H16V6C16 5.44772 15.5523 5 15 5Z" fill="currentColor"/>
             </svg>
           </a>
         </div>
